3be765029c62d5f9262c80ea63ac25b095c52bb7
Asif <asifrmemon@gmail.com>
2016-09-02 07:31:33 -0700

    Modified the way we get template and how we call the functions.

--- script.js
+++ script.js
@@ -2,23 +2,32 @@
 function domobj(){
   var self        = this;
   self.products   = [];
+  self.template   = null;
 
-  //getting the products response data from data.json and pushing them to products array.
-  self.getproducts = function(url, callback){
+  //getting the response data from data.json creating new productobj and pushing them to self.products array. Then running gettemplate
+  self.getproducts = function(url){
     $.getJSON(url, function(response){
-        for(var i=0; i<response.sales.length ; i++){
-          self.products.push( new productobj(response.sales[i], i)  );
-        }
-        callback();
-    });
+      for(var i = 0; i < response.sales.length; i++){
+        self.products.push( new productobj(response.sales[i], i)  );
+      }
+      self.gettemplate(); 
+    }) 
   }
   
-  //getting the updated html for each product  
-  self.updateproducthtml = function(callback){
-    for(var i = 0; i < self.products.length ; i++){
-      self.products[i].updatehtml();
-    }
-    callback();
+  //getting the product template then running updateproducthtml
+  self.gettemplate = function(){
+    $.get("product-template.html", function(template){
+      self.template = template;
+      self.updateproducthtml();
+    })
+  }  
+
+  //getting the updated html for each product then running updatedom  
+  self.updateproducthtml = function(){
+      for(var i = 0; i < self.products.length ; i++){
+        self.products[i].updatehtml(self.template);
+      }
+      self.updatedom();
   }
   
   //updating the dom by filling it with the products html
@@ -29,12 +38,11 @@ function domobj(){
       thishtml += self.products[i].htmlview;
       if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";}
     }
-
-    $("#content").append(thishtml)
+    $("#content").append(thishtml);
 
     //delete a product when the x is clicked
-    $('.close-button').click(function(){
-      $(this).parent().hide('slow');
+    $(".close-button").click(function(){
+      $(this).parent().hide("slow");
     })
   }
   
@@ -43,28 +51,23 @@ function domobj(){
 //defining product objects attributes
 function productobj(product, i){
   var self                  = this;
-  self.photo                = product.photos.medium_half
-  self.title                = product.name
-  self.tagline              = product.tagline
-  self.url                  = product.url
-  self.htmlview             = ''
-  self.index                = i
-  self.custom_class         = "col"+ ((i % 3) +1)
-  self.product_description  = product.description
+  self.photo                = product.photos.medium_half;
+  self.title                = product.name;
+  self.tagline              = product.tagline;
+  self.url                  = product.url;
+  self.htmlview             = "";
+  self.index                = i;
+  self.custom_class         = "col"+ ((i % 3) +1);
+  self.product_description  = product.description;
   
   //filling product-template with product object attributes
   self.updatehtml = function(template){
-    $.get('product-template.html', function(template){
-        self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class).replace('{product_description}', self.product_description);
-      })
+    self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class).replace('{product_description}', self.product_description);
   }
   
 }
 
-//creating new dom object, running getproducts, then calling back updateproducthtml, then running updatedom after a short delay.
+//creating new dom object, running getproducts.
 var page = new domobj();
-page.getproducts('data.json', function(){
-  page.updateproducthtml(function(){
-    setTimeout("page.updatedom();",50);
-  });
-})
+page.getproducts('data.json')
+
